c80f17bf1c42c8207a7f49c7dae86dc0d5018053
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2024-09-04 00:17:40 -0300

    added: gravity, jump, floor

--- entrega_3_lopezgaxel/entities/player/player.gd
+++ entrega_3_lopezgaxel/entities/player/player.gd
@@ -1,5 +1,12 @@
-extends Sprite2D
-@export var speed:int = 600 # How fast the player will move (pixels/sec).
+extends CharacterBody2D
+@export var ACCELERATION:float = 20.0
+@export var H_SPEED_LIMIT:float = 600.0
+@export var J_SPEED:float = -500.0
+@export var G_Force:float = 5.0
+@export var FRICTION_WEIGHT:float = 0.1
+
+var up:Vector2 = Vector2.UP
+var vel:Vector2 = Vector2.ZERO
 var cannon:Sprite2D
 var screen_size # Size of the game window.
 var bullet_container:Node
@@ -8,7 +15,19 @@ func _ready():
 	cannon = $Cannon
 
 func _process(delta):
-	position.x += (int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left"))) * speed * delta
+	var mov_dir:int = (int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left")))
+	
+	if mov_dir != 0:
+		velocity.x = clamp(velocity.x + (mov_dir * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = J_SPEED
+	
+	velocity.y += G_Force
+	move_and_slide()#no requiere parametro
+	#position += velocity * delta
+	
 	var mouse_pos:Vector2 = get_global_mouse_position()
 	cannon.look_at(mouse_pos)
 	if Input.is_action_just_pressed("Fire"):
